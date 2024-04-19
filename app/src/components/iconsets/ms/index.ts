b122e51951ba080277e88e8a20010c191ad802c8
Ryan Nguyen <tuannguyen7326.dev@gmail.com>
2024-04-20 03:25:48 +0700

    -

--- app/src/components/iconsets/ms/index.ts
+++
@@ -1,5 +0,0 @@
-import MaterialSymbolIcon from './MaterialSymbolIcon.vue';
-
-export const ms = {
-  component: MaterialSymbolIcon,
-};
--- app/src/services/posts_service/posts_fetcher.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-
-export const postsFetcher = axios.create({
-  baseURL: '/posts',
-});
